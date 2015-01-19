59063c013cf8d86e969dfe9d5f4ac361b02f89c5
iquintasgonzalez <iquintasgonzalez@eniac02.danielcastelao.org>
2015-01-19 08:54:50 +0100

    falta algo

---
+++ src/array2/Array2.java
@@ -0,0 +1,14 @@
+
+package array2;
+
+
+public class Array2 {
+
+    
+    public static void main(String[] args) {
+        String[]nomeXog={"aa","bb","cc","dd","ee"};
+        int[]dorsais={5,7,9,2,1};
+        metodos.visualizar(nomeXog, dorsais);
+    }
+    
+}
---
+++ src/array2/metodos.java
@@ -0,0 +1,33 @@
+
+package array2;
+
+/**
+ *
+ * @author iquintasgonzalez
+ */
+public class metodos {
+    public static void visualizar(String[]nome,int[]dorsais){
+        System.out.println("nome"+"dorsal");
+        for(int i=0;i<nome.length;i++){
+            System.out.println(nome[i]+"\t\t"+dorsais[i]);
+        }
+    }
+    public static void buscar (String []nome,int[]dorsais,int elebus){
+        int atopado=0;//non está
+        for(int i=0;i<nome.length;i++){
+            if(elebus==dorsais[i])
+                atopado=1;
+            System.out.println("nome atopado"+nome[i]);
+            break;
+        }
+    
+    if atopado=0;
+        System.out.println("non esta ese xogador");
+        break;
+    
+}
+
+
+}
+
+
